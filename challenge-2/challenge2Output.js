17bafb9a6c4dce2d44295ea35fca46ce674ed362
Gareth Devlin <garethlouisdevlin@gmail.com>
2026-03-05 17:06:51 +0000

    Update challenge-2/challenge2Output.js

--- challenge-2/challenge2Output.js
+++ challenge-2/challenge2Output.js
@@ -2,7 +2,7 @@ import { challenge2Input } from "./challenge2input.js";
 // node challenge-2/challenge2Output.js 1
 // node challenge-2/challenge2Output.js 2
 
-// Inline fixtures for quick manual checks while iterating locally.
+// Inline test fixtures for quick manual validation.
 const testInput =
   "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 const testInputShort = "11-22";
